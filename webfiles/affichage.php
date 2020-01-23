276c2f73225999b76c4795b5ef7325ab7b7d3ad7
root <root@localhost.localdomain>
2020-01-23 10:11:39 +0100

    ajout manuel de affichage.php

---
+++ webfiles/affichage.php
@@ -0,0 +1,97 @@
+
+
+<?php
+
+$servername = "db-service";
+$username = "julien";
+$password = "password";
+$dbname = "train";
+#$server="10.69.8.33";
+#$user="julien";
+#$pass="password";
+#$db="train";
+
+
+
+$link = mysqli_connect($servername, $username, $password);
+if (!$link) {
+   die('Impossible de se connecter : ' . mysqli_error());
+}
+
+// Rendre la base de données foo, la base courante
+$db_selected = mysqli_select_db($link, $dbname);
+if (!$db_selected) {
+   die ('Impossible de sélectionner la base de données : ' . mysqli_error());
+}
+
+
+
+// C'est la meilleur façon d'exécuter une requête SQL
+// Pour plus d'exemples, voir mysql_real_escape_string()
+
+$query = "SELECT id_TGV as tid, gd.nom_gare as ngd, ga.nom_gare as nga, Date_depart, Date_arrivee FROM tgv t, gares gd, gares ga WHERE t.id_gare_depart=gd.id_gare AND t.id_gare_arrivee=ga.id_gare ORDER BY Date_depart" ;  
+
+// Exécution de la requête
+$result = mysqli_query($link, $query);
+
+// Vérification du résultat
+// Ceci montre la requête envoyée à MySQL ainsi que l'erreur. Utile pour déboguer.
+if (!$result) {
+    $message  = 'Requête invalide : ' . mysqli_error() . "\n";
+    $message = 'Requête complète : ' . $query;
+    die($message);
+}
+
+// Utilisation du résultat
+// Tenter d'affichager $result ne vous donnera pas d'informations contenues dans la ressource
+// Une des fonctions MySQL de résultat doit être utilisée
+// Voir aussi mysql_result(), mysql_fetch_array(), mysql_fetch_row(), etc.
+
+echo "
+<style>
+
+table,
+td {
+    border: 1px solid #333;
+}
+
+thead,
+tfoot {
+    background-color: #333;
+    color: #fff;
+}
+
+</style>";
+
+echo "<table>
+    <thead>
+        <tr>
+            <th colspan=\"5\">Planning des trains</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td> Train n° </id>
+            <td> Heure de départ </td>
+            <td> Heure d'arrivée</td>
+            <td> Ville de départ </td>
+            <td> Ville d'arrivée</td>
+        </tr>";
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>".$row['tid']."</td>";
+    echo "<td>".$row['Date_depart']."</td>";
+    echo "<td>".$row['Date_arrivee']."</td>";
+    echo "<td>".$row['ngd']."</td>";
+    echo "<td>".$row['nga']."</td>";
+    echo "</tr>";
+
+}
+echo"</tbody>
+</table>";
+
+
+mysqli_free_result($result);
+
+?>
